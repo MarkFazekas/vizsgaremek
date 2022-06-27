b17dbab7578fed6a0ac7ed133187b06dabb03ac6
Márk Fazekas <coldrex.fazekas.mark@gmail.com>
2022-06-27 09:40:12 +0200

    Backend V0.1.4.1

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -2,5 +2,7 @@ module.exports = {
     coverageProvider: "v8",
     testEnvironment: "node",
     modulePathIgnorePatterns: ["<rootDir>/.*/__mocks__"],
-    testTimeout: 600000
+    testTimeout: 600000,
+    "maxWorkers": 1,
+    "verbose": true,
 };
